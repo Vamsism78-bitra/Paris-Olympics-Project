6c53d6cbfb4a434f83d6e19bebce317a6c9fea61
BHANU NAGA SAI VAMSI BITRA <BHANU.BITRA.2025@mumail.ie>
2026-04-11 20:29:34 +0100

    add little code

--- aap.py
+++ aap.py
@@ -1,1 +1,3 @@
 print("hello world sample code") 
+print("This is a sample code for the Paris Olympics project.")
+print("We will be working on various aspects of the Olympics, including event scheduling, athlete management, and medal tracking.")
